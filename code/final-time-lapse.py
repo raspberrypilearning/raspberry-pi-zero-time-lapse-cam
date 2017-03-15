25c8ce14fec2f46852cab3cc54b557eff901140a
Steveis <SteveAmor@users.noreply.github.com>
2017-03-15 19:12:18 +0000

    Update final-time-lapse.py
    Updated to keep it consistent with the code in the worksheet.

--- code/final-time-lapse.py
+++ code/final-time-lapse.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 import picamera
 
 WAIT_TIME = 30
@@ -6,4 +6,4 @@ WAIT_TIME = 30
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     for filename in camera.capture_continuous('/home/pi/time-lapse/img{timestamp:%H-%M-%S-%f}.jpg'):
-        time.sleep(WAIT_TIME)
+        sleep(WAIT_TIME)
